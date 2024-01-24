114b95feedb05926f01327ce2c1ebb4e090b1dad
Pavel Buryka <l1kaf@yandex.ru>
2024-01-24 21:59:50 +0300

    add main.phtml

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/L1kaf/php-project-9/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/L1kaf/php-project-9/actions)
 [![PHP linter](https://github.com/L1kaf/php-project-9/actions/workflows/main.yml/badge.svg)](https://github.com/L1kaf/php-project-9/actions/workflows/main.yml)
+[![Maintainability](https://api.codeclimate.com/v1/badges/acb6bde531a170866f9e/maintainability)](https://codeclimate.com/github/L1kaf/php-project-9/maintainability)
 
 ### Description:
 ---
--- public/index.php
+++ public/index.php
@@ -3,15 +3,21 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
+    return $this->get('renderer')->render($response, 'main.phtml');
 });
 
-$app->get('/test', function ($request, $response) {
+$app->get('/urls', function ($request, $response) {
     return $response->write('Welcome to test');
 });
 $app->run();
