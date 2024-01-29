80488fa2d4b922050a20776d002a3d213f6bdbb6
Pavel Buryka <l1kaf@yandex.ru>
2024-01-29 17:52:15 +0300

    add Connection.php and CreateTables.php

--- public/index.php
+++ public/index.php
@@ -4,10 +4,16 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use DI\Container;
+use App\Connection;
 
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$container->set('connection', function () {
+    $pdo = Connection::get()->connect();
+    return $pdo;
 });
 
 $app = AppFactory::createFromContainer($container);
@@ -15,9 +21,10 @@ $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'main.phtml');
-});
+})->setName("index");
 
 $app->get('/urls', function ($request, $response) {
     return $this->get('renderer')->render($response, 'show.phtml');
-});
+})->setName("urls.store");
+
 $app->run();
---
+++ src/CreateTables.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+class CreateTables
+{
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function createTableUrls()
+    {
+        $sql = 'CREATE TABLE IF NOT EXISTS urls (
+                    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                    name varchar(255),
+                    created_at timestamp
+        );';
+        
+        $this->pdo->exec($sql);
+
+        return $this;
+    }
+}
