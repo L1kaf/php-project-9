158dd8272942d5e48c7363861bf953b4db28a785
Pavel Buryka <l1kaf@yandex.ru>
2024-01-30 20:22:24 +0300

    update Connection.php

--- src/Connection.php
+++ src/Connection.php
@@ -7,7 +7,18 @@ final class Connection
     private static ?Connection $conn = null;
     public function connect()
     {
-        $params = parse_ini_file('database.ini');
+        if (getenv('DATABASE_URL')) {
+            $databaseUrl = parse_url(getenv('DATABASE_URL'));
+        }
+        if (isset($databaseUrl['host'])) {                                                 
+            $params['host'] = $databaseUrl['host'];
+            $params['port'] = isset($databaseUrl['port']) ? $databaseUrl['port'] : null;
+            $params['database'] = isset($databaseUrl['path']) ? ltrim($databaseUrl['path'], '/') : null;
+            $params['user'] = isset($databaseUrl['user']) ? $databaseUrl['user'] : null;
+            $params['password'] = isset($databaseUrl['pass']) ? $databaseUrl['pass'] : null;
+        } else {
+            $params = parse_ini_file('database.ini');
+        }
         if ($params === false) {
             throw new \Exception("Error reading database configuration file");
         }
@@ -20,7 +31,7 @@ final class Connection
             $params['user'],
             $params['password']
         );
-
+        
         $pdo = new \PDO($conStr);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
